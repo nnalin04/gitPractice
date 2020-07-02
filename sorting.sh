060af33baf484bde3a755a3c8b97a8433d41f2fd
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-02 23:25:59 +0530

    sorting UC6

--- sorting.sh
+++ sorting.sh
@@ -1,4 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
+
+declare -A sorting
+declare -a arr
+
 
 read -p "enter number : " a
 read -p "enter number : " b
@@ -11,3 +15,8 @@ x=`echo $a $b $c | awk '{print $1%$2+$3}'`
 y=`echo $a $b $c | awk '{print $3+$1/$2}'`
 
 z=`echo $a $b $c | awk '{print $1*$2+$3}'`
+
+sorting[c1]=$w
+sorting[c2]=$x
+sorting[c3]=$y
+sorting[c4]=$z
