0b9e88774d3a06786b3561aabb5f09e8d4a17ca2
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-02 23:15:06 +0530

    UC4

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -30,3 +30,38 @@ then
     dif=$(($countT-$countH))
     echo "Tails came $dif times more the Heads"
 fi
+
+if [ $countH -eq $countT ]
+then
+    while [ dif -gt 2 ]
+    do
+        r=$((RANDOM%2))
+        count=$(($count+1))
+        if [ $r -eq 0 ]
+        then 
+            countH=$(($countH+1))
+        fi
+        if [ $r -eq 1 ]
+        then
+            countT=$(($countT+1))
+        fi
+
+        if [ $countH -gt $countT ]
+        then
+            dif=$(($countH-$countT))
+        elif [ $countT -gt $countH ]
+        then
+            dif=$(($countT-$countH))
+        fi
+    done
+    if [ $countH -gt $countT ]
+    then
+        dif=$(($countH-$countT))
+        echo "Heads came $dif times more the Tails"
+    elif [ $countT -gt $countH ]
+    then
+        dif=$(($countT-$countH))
+        echo "Tails came $dif times more the Heads"
+    fi
+fi
+
